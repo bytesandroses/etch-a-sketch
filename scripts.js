2e5d9e28c919b818cf6b8d5d7dcbd07c2ad46eb4
Isaac <59330021+bytesandroses@users.noreply.github.com>
2024-06-18 00:30:08 +0200

    Add event listener so that cells change color when mouse hovers over them

--- scripts.js
+++ scripts.js
@@ -1,14 +1,25 @@
 grid = document.querySelector("#grid");
-console.log(grid); // Add this line
 
 function createGrid() {
   for (let row = 0; row < 16; row++) {
     for (let col = 0; col < 16; col++) {
       const cell = document.createElement("div");
+      const cellNumber = "cell" + ((row * 16) + col);
       cell.classList.add("cell");
+      cell.id = cellNumber;
       grid.appendChild(cell);
     }
   }
 }
 
 createGrid();
+
+cell = document.querySelectorAll(".cell");
+
+cell.forEach(cell => { cell.addEventListener(
+  "mouseenter",
+  (event) => {
+    event.target.style.backgroundColor = "green";
+  }
+)  
+});
